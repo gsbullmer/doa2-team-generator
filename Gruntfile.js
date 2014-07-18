7d675fc4bcced77603409337184aa11a28294790
gsbullmer <gsbullmer@gmail.com>
2014-07-18 11:09:38 -0500

    Build tools update

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,6 +69,7 @@ module.exports = function (grunt) {
       prod: {
         options: {
           generateSourceMaps: true,
+          preserveLicenseComments: false,
           optimize: 'uglify2'
         }
       }
